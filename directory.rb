92cf704620237d7c86f2c7e4e5c77d7a165947c8
Simonbao <Simonbao.sb@gmail.com>
2017-12-12 21:06:11 +0000

    Program refactored and basic scalibility implemented.

--- directory.rb
+++ directory.rb
@@ -1,22 +1,23 @@
 #Creation a Array which stores all students and deleted student_count
 students = [
- 'Dr. Hannibal Lecter',
- 'Darth Vader',
- 'Nurse Ratched',
- 'Micheal Corleone',
- 'Alex DeLarge',
- 'The Wicked Witch of the West',
- 'Terminator',
- 'Freddy Kruger',
- 'The Joker',
- 'Joffrey Baratheon',
- 'Norman Bates'
+ {name: 'Dr. Hannibal Lecter', cohort: 'November'},
+ {name: 'Darth Vader', cohort: 'November'},
+ {name: 'Nurse Ratched', cohort: 'November'},
+ {name: 'Micheal Corleone', cohort: 'November'},
+ {name: 'Alex DeLarge', cohort: 'November'},
+ {name: 'The Wicked Witch of the West', cohort: 'November'},
+ {name: 'Terminator', cohort: 'November'},
+ {name: 'Freddy Krueger', cohort: 'November'},
+ {name: 'The Joker', cohort: 'November'},
+ {name: 'Joffrey Baratheon', cohort: 'November'},
+ {name: 'Norman Bates', cohort: 'November'}
 ]
 
 
-#Prints out the total amount of students enrolled, the number is still static but now attached to a variable and can be easily changed.
+#Prints out the total amount of students enrolled, the number is still static but
+#now attached to a variable and can be easily changed.
 #The code also got refactored and student count is now interpolation is used.
 puts 'The students of Villains Academy'
 puts '-------------'
-students.each { |student| puts student  }
+students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
 print "Overall, we have #{students.count} great students.\n"
