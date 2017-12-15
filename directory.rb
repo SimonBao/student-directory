842260412d4cd09dfe998ce25879ed219c5c3706
Simonbao <Simonbao.sb@gmail.com>
2017-12-15 18:01:41 +0000

    Addition of Indexes

--- directory.rb
+++ directory.rb
@@ -1,23 +1,39 @@
-#Creation a Array which stores all students and deleted student_count
-students = [
- {name: 'Dr. Hannibal Lecter', cohort: 'November'},
- {name: 'Darth Vader', cohort: 'November'},
- {name: 'Nurse Ratched', cohort: 'November'},
- {name: 'Micheal Corleone', cohort: 'November'},
- {name: 'Alex DeLarge', cohort: 'November'},
- {name: 'The Wicked Witch of the West', cohort: 'November'},
- {name: 'Terminator', cohort: 'November'},
- {name: 'Freddy Krueger', cohort: 'November'},
- {name: 'The Joker', cohort: 'November'},
- {name: 'Joffrey Baratheon', cohort: 'November'},
- {name: 'Norman Bates', cohort: 'November'}
-]
+#input_students prompts user guide and prompts for a student name
+#name obtained is pushed to students Array in a hash format with the current cohort
+def input_students
+  puts 'Please enter the name of the students.'
+  puts 'To finish, just hit return twice.'
+  students = []
+  name = gets.chomp
+  #until name is empty it keeps running. 'until name.empty?' also could be used.
+  while !name.empty? do
+    students << {name: name, cohort: 'november'}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
+def print_header
+  puts 'The students of Villains Academy'
+  puts '-------------'
+end
+
+def print_footer(students)
+  print "Overall, we have #{students.count} great students.\n"
+end
+
+def prints(students)
+  # each method iterates through each student in students Array.
+  # and puts their name and cohort out.
+  students.each_with_index do |student, idx|
+    puts "#{idx+1}.#{student[:name].capitalize} (#{student[:cohort].capitalize} cohort)"
+  end
+end
 
 
-#Prints out the total amount of students enrolled, the number is still static but
-#now attached to a variable and can be easily changed.
-#The code also got refactored and student count is now interpolation is used.
-puts 'The students of Villains Academy'
-puts '-------------'
-students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
-print "Overall, we have #{students.count} great students.\n"
+
+students = input_students
+print_header
+prints(students)
+print_footer(students)
