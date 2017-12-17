db0307f2810bc2d0f9f3c20e299af4b801087d7c
Simonbao <Simonbao.sb@gmail.com>
2017-12-17 14:47:20 +0000

    More questions added

--- directory.rb
+++ directory.rb
@@ -7,11 +7,22 @@ def input_students
   name = gets.chomp
   #until name is empty it keeps running. 'until name.empty?' also could be used.
   while !name.empty? do
-    students << {name: name, cohort: 'november'}
+    hobbies, country, height = more_info
+    students << {name: name, cohort: 'november', hobby: hobbies, origin: country, height: height}
     puts "Now we have #{students.count} students"
     name = gets.chomp
   end
   students
+end
+
+def more_info
+  print 'What is your hobby: '
+  hobby = gets.chomp
+  print "What is your country: "
+  country = gets.chomp
+  print "hat is your height in cm: "
+  height = gets.chomp
+  return hobby, country, height
 end
 
 def print_header
@@ -26,10 +37,15 @@ end
 def prints(students)
   # each method iterates through each student in students Array.
   # and puts their name and cohort out.
-  print 'Search by letter: '
-  starts_with = gets.chomp
+  # print 'Search by letter: '
+  # starts_with = gets.chomp
   students.each_with_index do |student, idx|
-    puts "#{idx+1}.#{student[:name].capitalize} (#{student[:cohort].capitalize} cohort)" if starts_with.match((student[:name])[0])
+    # puts "#{idx+1}.#{student[:name].capitalize} (#{student[:cohort].capitalize} cohort)"  if starts_with.match((student[:name])[0])
+    print "#{idx}."
+    student.each do |k, v|
+      print "#{k}: #{v}, "
+    end
+    puts
   end
 end
 
